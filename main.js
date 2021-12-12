bf60679d02c9acf578e8eaffdbd5a4a7e83fdfac
Che <cgoranov@gmail.com>
2021-12-12 14:45:20 -0600

    commit tree 2c9ce103ca75fb502e5e8df3fbd69b9c41f7e11a

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errModal = document.getElementById('modal')
+  errModal.className = 'hidden'
 
+document.addEventListener('DOMContentLoaded', () => {
+  
+
+  const hearts = document.getElementsByClassName('like-glyph')
+
+  for (const heart of hearts) {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+      .then(response => {
+        if (response) {
+          if (heart.innerText === '♡') {
+            heart.classList.add('activated-heart')
+            heart.innerText = FULL_HEART
+          } else {
+            heart.classList.remove('activated-heart')
+            heart.innerText = EMPTY_HEART
+          }
+        }
+      })
+      .catch(err => {
+        errModal.classList.remove('hidden')
+        errModal.children[1].innerText = err
+
+        setTimeout(() => {
+          errModal.classList.add('hidden') 
+        }, 3000)
+      })
+
+    })
+  }
+})
 
 
 
